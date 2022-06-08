adbbccb494fc186553a1e7c00f54b9ff46031930
cedric ngakam <cedricnga2@gmail.com>
2022-06-08 15:24:00 +0200

    README

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const MysteryBoxToken = artifacts.require("MysteryBoxToken");
 
 module.exports = function (deployer) {
-  deployer.deploy(MysteryBoxToken, "MysteryBox", "MBOX", 3600);
+  deployer.deploy(MysteryBoxToken, "MysteryBox", "MBOX", 432000);
 };
